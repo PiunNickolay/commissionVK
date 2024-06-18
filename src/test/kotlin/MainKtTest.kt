84e47e0664b959595a94a7d2c7fa40b62798d83e
PiunNickolay <147736400+PiunNickolay@users.noreply.github.com>
2024-06-18 23:19:25 +0300

    MainKtTest.kt

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -6,5 +6,12 @@ class MainKtTest {
 
     @Test
     fun commission() {
+        val cardType:String = "Mastercard"
+        val previousTransfersThisMonth = 0
+        val transferAmount = 50000
+
+        val result = commission(cardType, previousTransfersThisMonth, transferAmount)
+
+        assertEquals(320, result)
     }
 }
